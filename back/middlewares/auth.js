41d01bcf3cb6c8434df41036a1e9961d8c42bf40
YoannPitance <pittyo88@gmail.com>
2023-02-06 15:36:17 +0100

    5- correction faute de frappe decodedeToken! rest à gérer les likes

--- back/middlewares/auth.js
+++ back/middlewares/auth.js
@@ -3,8 +3,8 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const decodedeToken = jwt.verify(token, "SECRET_TOKEN");
-    const userId = decodedeToken.userId;
+    const decodedToken = jwt.verify(token, "SECRET_TOKEN");
+    const userId = decodedToken.userId;
     req.auth = {
       userId: userId,
     };
